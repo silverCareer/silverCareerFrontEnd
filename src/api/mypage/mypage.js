8bd268a49e29f6dfcb65192d71f3b6c9fa1d18ce
sss_hun <88662101+seonghunhan@users.noreply.github.com>
2023-08-08 10:57:48 +0900

    Feat mypage getapi (#14)
    * Chore: ReadMe 수정
    
    * Feat : 마이페이지API 적용 및 ContextAPI에서 Data파싱
    
    * Feat : 마이페이지 API 생성 및 contextAPI활용
    
    ---------
    
    Co-authored-by: Changho Sung <84298467+kylesung0520@users.noreply.github.com>
    Co-authored-by: kylesung0520 <kylesung0520@gmail.com>

--- src/api/mypage/mypage.js
+++ src/api/mypage/mypage.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const getMyProfile = async () => {
-    const token = localStorage.getItem('jwt'); 
-    const response = await axios.get('/api/mypage', {
+    const token = localStorage.getItem('jwttoken'); 
+    const response = await axios.get('https://www.silvercareer.shop/api/members', {
         headers: {
             'Authorization': `Bearer ${token}`
         }
