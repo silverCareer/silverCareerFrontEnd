d5d4424556eddc2e42e460fab56053dfa429cd1d
sss_hun <88662101+seonghunhan@users.noreply.github.com>
2023-08-17 14:44:21 +0900

    Feat : 채팅 테스트

--- src/utils/websocket.js
+++ src/utils/websocket.js
@@ -2,7 +2,7 @@ import { Client } from '@stomp/stompjs';
 import SockJS from 'sockjs-client';
 
 export const createWebSocketClient = () => {
-    const socket = new SockJS('http://43.201.132.241:8888/ws');  
+    const socket = new SockJS('https://43.201.132.241:8888/ws');  
     const client = new Client({
         webSocketFactory: () => socket,
         reconnectDelay: 5000,
