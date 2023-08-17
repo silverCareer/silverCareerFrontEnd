1ab05992bd9f5ddc609b532a8f49de47da13579b
sss_hun <88662101+seonghunhan@users.noreply.github.com>
2023-08-17 16:09:19 +0900

    Feat : 채팅테스트

--- src/utils/websocket.js
+++ src/utils/websocket.js
@@ -2,7 +2,7 @@ import { Client } from '@stomp/stompjs';
 import SockJS from 'sockjs-client';
 
 export const createWebSocketClient = () => {
-    const socket = new SockJS('https://43.201.132.241:8888/ws');  
+    const socket = new SockJS('https://www.silvercareer.shop/ws');  
     const client = new Client({
         webSocketFactory: () => socket,
         reconnectDelay: 5000,
@@ -10,6 +10,8 @@ export const createWebSocketClient = () => {
 
     return client;
 };
+
+//https://43.201.132.241:8888/ws
 
 //'http://localhost:8888/ws'
 //'https://www.silvercareer.shop/ws'
