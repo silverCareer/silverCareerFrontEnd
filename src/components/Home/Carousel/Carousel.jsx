2a0d02bbf536371d251aec81dfcdeecf71068393
sss_hun <88662101+seonghunhan@users.noreply.github.com>
2023-08-25 13:00:35 +0900

    Feat : 캐러셀 수정

--- src/components/Home/Carousel/Carousel.jsx
+++ src/components/Home/Carousel/Carousel.jsx
@@ -1,23 +1,29 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState, useContext } from 'react';
 import styles from './carousel.module.css';
 import { ReactComponent as LeftIcon } from '../../../assets/svg/icon-left.svg';
 import { ReactComponent as RightIcon } from '../../../assets/svg/icon-right.svg';
+import { useNavigate } from 'react-router-dom'; // for navigating
+import { ProductDetailContext } from '../../../hooks/productDetailContext';
+import { getProductDetail } from '../../../api/product/productDetail';
 
 const HomeCarousel = ({ carouselList }) => {
-    const [currIndex, setCurrIndex] = useState(0);
+    const [currIndex, setCurrIndex] = useState(1);
     const [currList, setCurrList] = useState([]);
     const [totalImages, setTotalImages] = useState(0);
-
     const carouselRef = useRef(null);
+
+    const navigate = useNavigate(); 
+
+    const { setProductDetailInfo } = useContext(ProductDetailContext);
 
     useEffect(() => {
         if (carouselList.length !== 0) {
             const startData = carouselList[0];
             const endData = carouselList[carouselList.length - 1];
             const newList = [endData, ...carouselList, startData];
-
+    
             setCurrList(newList);
-            setCurrIndex(1); // Change to 1 to display the first image initially
+            setTotalImages(newList.length - 2);
         }
     }, [carouselList]);
 
@@ -27,22 +33,6 @@ const HomeCarousel = ({ carouselList }) => {
             carouselRef.current.style.transition = 'all 0.5s ease-in-out';
         }
     }, [currIndex]);
-
-    useEffect(() => {
-        if (carouselList.length !== 0) {
-            const startData = carouselList[0];
-            const endData = carouselList[carouselList.length - 1];
-            const newList = [endData, ...carouselList, startData];
-    
-            setCurrList(newList);
-            setCurrIndex(1); // Change to 1 to display the first image initially
-            setTotalImages(newList.length - 2);
-        }
-    }, [carouselList]);
-
-    const moveToNthSlide = (index) => {
-        setCurrIndex(index);
-    };
 
     const handleSwipe = (direction) => {
         const newIndex = currIndex + direction;
@@ -56,21 +46,51 @@ const HomeCarousel = ({ carouselList }) => {
         }
     };
 
+    const moveToNthSlide = (index) => {
+        setCurrIndex(index);
+    };
+
+
+
+    const navigateToProductDetail = async (productIdx) => {
+        try {
+            const productDetailResponse = await getProductDetail(productIdx);
+
+            if (productDetailResponse.success) {
+                setProductDetailInfo(productDetailResponse.response);
+                navigate(`/product/${productIdx}`);
+            } else {
+                console.error("Failed to fetch product Detail:", productDetailResponse.error);
+            }
+        } catch (error) {
+            console.error("Error fetching product Detail:", error);
+        }
+    };
+
     return (
         <div className={styles.container}>
-            <div className={styles.carouselWrapper}>
-                <ul className={styles.carousel} ref={carouselRef}>
-                    {currList.map((image, idx) => {
-                        const key = `${image}-${idx}`;
-
-                        return (
-                            <li key={key} className={styles.carouselItem}>
-                                <img src={image} alt={`carousel-img-${idx}`} />
-                            </li>
-                        );
-                    })}
-                </ul>
-            </div>
+            <span className={`${styles.titleText}`}>가장 인기있는 Best 10</span>
+        <div className={styles.carouselWrapper}>
+            <ul className={styles.carousel} ref={carouselRef}>
+                {currList.map((item, idx) => {
+                    const key = `${item.productIdx}-${idx}`;
+                    return (
+                        <li key={key} className={styles.carouselItem}>
+                            <img src={item.productImage} alt={`carousel-img-${idx}`} />
+                            <div className={styles.productInfo}>
+                                <span className={styles.category}>{item.category}</span>
+                                <span className={styles.productName}>{item.productName}</span>
+                                <button 
+                                    className={styles.enrollButton} 
+                                    onClick={() => navigateToProductDetail(item.productIdx)}>
+                                    수강신청하러가기
+                                </button>
+                            </div>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
             <div className={styles.carouselIndex}> 
                 <button type="button" className={styles.swipeLeft} onClick={() => handleSwipe(-1)}>
                     <LeftIcon />
